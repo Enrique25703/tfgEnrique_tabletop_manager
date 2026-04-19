369c59b3ea431a79c3e67566d924866caa9c1779
Enrique Silveira Garcia <esilgar2507@gmail.com>
2026-04-19 11:53:13 +0200

    Primer commit del TFG

---
+++ src/main/java/org/example/tfgenrique/TfgEnriqueApplication.java
@@ -0,0 +1,13 @@
+package org.example.tfgenrique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfgEnriqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TfgEnriqueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tfgenrique/TfgEnriqueApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tfgenrique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgEnriqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
